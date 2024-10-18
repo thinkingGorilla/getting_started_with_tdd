71d12077b7431d57a4a04735b660c80cd626366e
thinkingGorilla <4zxzxz4@gmail.com>
2024-10-18 23:35:54 +0900

    Chapter 02 TDD 시작(PasswordStrengthMeter 16)
    - 복잡한 if-eles 대신 정수 카운터로 암호 강도를 대신하도록 코드 리팩토링

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -3,28 +3,13 @@ package chap02;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        int meetCounts = 0;
+        if (s.length() >= 8) meetCounts++;
+        if (meetsContainingNumberCriteria(s)) meetCounts++;
+        if (meetsContainingUppercaseCriteria(s)) meetCounts++;
 
-        if (lengthEnough && !containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-
-        if (!lengthEnough && containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-
-        if (!lengthEnough && !containsNum && containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-
-        if (!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-
-        if (!containsNum) return PasswordStrength.NORMAL;
-        if (!containsUpp) return PasswordStrength.NORMAL;
+        if (meetCounts == 1) return PasswordStrength.WEAK;
+        if (meetCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
