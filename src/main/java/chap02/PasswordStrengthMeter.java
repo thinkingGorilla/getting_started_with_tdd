2ad4db94072fad94984d417577b8f8432c71d4c1
thinkingGorilla <4zxzxz4@gmail.com>
2024-10-18 23:41:02 +0900

    Chapter 02 TDD 시작(PasswordStrengthMeter 17)
    - 아무 조건도 충족하지 않는 암호 강도 약함에 대한 테스트 코드 및 기능 구현

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -3,13 +3,13 @@ package chap02;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        int meetCounts = 0;
-        if (s.length() >= 8) meetCounts++;
-        if (meetsContainingNumberCriteria(s)) meetCounts++;
-        if (meetsContainingUppercaseCriteria(s)) meetCounts++;
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
 
-        if (meetCounts == 1) return PasswordStrength.WEAK;
-        if (meetCounts == 2) return PasswordStrength.NORMAL;
+        if (metCounts <= 1) return PasswordStrength.WEAK;
+        if (metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -80,4 +80,10 @@ public class PasswordStrengthMeterTest {
         // 단, 대문자 포함 조건만을 충족하는 경우에 대한 테스트를 작성하자.
         assertStrength("ABZEF", PasswordStrength.WEAK);
     }
+
+    @Test
+    void meetsNoCriteria_Then_Weak() {
+        // 아무 조건도 충족하지 않는 경우에 대한 테스트를 작성하자.
+        assertStrength("abc", PasswordStrength.WEAK);
+    }
 }
