c12aef21995da9f2e26b88d0ef81ce891cc314f6
thinkingGorilla <4zxzxz4@gmail.com>
2024-10-18 23:13:59 +0900

    Chapter 02 TDD 시작(PasswordStrengthMeter 10)
    - 문자열이 대문자를 포함하는지 판단하는 코드 리팩토링

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -11,17 +11,19 @@ public class PasswordStrengthMeter {
         boolean containsNum = meetsContainingNumberCriteria(s);
         if (!containsNum) return PasswordStrength.NORMAL;
 
-        boolean containsUpp = false;
-        for (char ch : s.toCharArray()) {
-            if (Character.isUpperCase(ch)) {
-                containsUpp = true;
-                break;
-            }
-        }
-
+        boolean containsUpp = meetsContainingUppercaseCriteria(s);
         if (!containsUpp) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean meetsContainingNumberCriteria(String s) {
