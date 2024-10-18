b1c12750aec42db56d027a7e44ee81c88bf1237f
thinkingGorilla <4zxzxz4@gmail.com>
2024-10-18 22:30:42 +0900

    Chapter 02 TDD 시작(PasswordStrengthMeter 1)
    - 암호 검사기 구현을 위한 테스트 코드 작성

---
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,18 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+public class PasswordStrengthMeterTest {
+
+    // - 검사할 규칙은 다음 세 가지이다.
+    //   1. 길이가 8글자 이상
+    //   2. 0부터 9 사이의 숫자를 포함
+    //   3. 대문자 포함
+    // - 세 규칙을 모두 충족하면 암호는 강함이다.
+    // - 2개의 규칙을 모두 충족하면 암호는 보통이다.
+    // - 1개 이하의 규칙을 충족하면 암호는 약함이다.
+    @Test
+    void name() {
+
+    }
+}
